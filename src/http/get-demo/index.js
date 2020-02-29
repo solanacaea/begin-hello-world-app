bc9596f89afa865c951cc8c5ebfd6bbc1d1c173a
solanacaea <43092492+solanacaea@users.noreply.github.com>
2020-02-29 15:30:22 +0800

    Update index.js

--- src/http/get-demo/index.js
+++ src/http/get-demo/index.js
@@ -1,27 +1,65 @@
-let data = require('@begin/demo');
-var option = {
-  table:"test",
-  key:"content"
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+var MongoClient = require('mongodb').MongoClient;
+var pass = process.env.PASS;
+var url = `mongodb://admin:${pass}@ds064299.mlab.com:64299/db1`
+var conn = function(url){
+  return new Promise((resolve,reject)=>{
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+      if (err) reject(err);
+      else resolve(db);
+    })
+  })
+}
+var toArray = function(data){
+  return new Promise((resolve,reject)=>{
+    data.toArray((err,res)=>{
+      if(err)reject(err)
+      else resolve(res);
+    })
+  })
 }
 
-exports.handler = async function http(req) {
-  var param = req.queryStringParameters;
-  if(param && param.content){
-    await data.set({...option,content:param.content});
+exports.handler = async (event, context) => {
+  try {
+    var con = await conn(url);
+    var db = con.db('db1');
+    var col = db.collection('test');
+    var res = await toArray(col.find({}));
     return {
-      headers: {
-        'content-type': 'text/html; charset=utf8'
-      },
-      body: "ok"
-    }
-  }else{
-    var res = await data.get({...option});
-    return {
-      headers: {
-        'content-type': 'text/html; charset=utf8'
-      },
+      statusCode: 200,
       body: JSON.stringify(res)
     }
-  
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
   }
+}
+
+
+// HTTP function
+exports.handler = async function http(req) {
+  // queryStringParameters 
+  // body
+  // path
+  // headers
+  console.log(req)
+  
+  try {
+    var con = await conn(url);
+    var db = con.db('db1');
+    var col = db.collection('test');
+    var res = await toArray(col.find({}));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res)
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+  
+//   return {
+//     headers: {
+//       'content-type': 'text/html; charset=utf8'
+//     },
+//     body: html
+//   }
 }
